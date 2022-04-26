4f64132f32be99fd63ba5dfb1c132b8d70ea5db6
Grothendieck57 <88466596+Grothendieck57@users.noreply.github.com>
2022-04-25 20:02:42 -0500

    Add files via upload

--- interviewPost.go
+++ interviewPost.go
@@ -7,8 +7,8 @@ import (
 	"log"
 	"net/http"
 	"net/url"
-	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -27,12 +27,10 @@ func main() {
 	json.Unmarshal([]byte(s2), &items) //Yep, it's time to pull the data out and stuff it into our array.
 	data := url.Values{}               //We will stuff all of the string elements of the array intot his before attempting to enconde and post at `submit` EP
 	i := 0                             //This is our loop control counter.
-
 	//Now let's feed the String array into data in-order to interatively POST to the endpoint.
 	for i < len(items) {
 		pItem1 := fmt.Sprintf("%v,", items[i]) //Payload item 1
 		data.Set("importantData", pItem1)      //Let's start giving `data` some meat
-
 		//Are we going beyond the bounds of `items`? If not, fill the second element of the payload with the next string. Otherwise, fill with `#`
 		if i+1 < len(items) {
 			pItem2 := fmt.Sprintf("%v,", items[i+1]) //Payload item 2 is the next string
@@ -43,19 +41,27 @@ func main() {
 		}
 		//Same as above conditional
 		if i+2 < len(items) {
-			pItem3 := fmt.Sprintf("%v", items[i+2])
+			pItem3 := fmt.Sprintf("%v,", items[i+2])
 			data.Add("importantData", pItem3)
 		} else {
 			pItem3 := "#"
 			data.Add("importantData", pItem3)
 		}
+		if i+3 < len(items) {
+			pItem4 := fmt.Sprintf("%v", items[i+3])
+			data.Add("importantData", pItem4)
+		} else {
+			pItem4 := "#"
+			data.Add("importantData", pItem4)
+		}
+		time.Sleep(500 * time.Millisecond) //Let's make a POST request every 500 milliseconds. From testing, the server seems to be happy with this rate. 
 		client := &http.Client{}
 		r, err := http.NewRequest("POST", endpoint, strings.NewReader(data.Encode())) // URL-encoded payload
 		if err != nil {
 			log.Fatal(err)
 		}
 		r.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
+		//r.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
 
 		res, err := client.Do(r)
 		if err != nil {
@@ -63,7 +69,7 @@ func main() {
 		}
 		log.Println(res.Status)
 		defer res.Body.Close()
-		i = i + 3 //Given that we have used three consecutive string elements, skip forward to avoid stuffing in duplicate strings.
+		i = i + 4 //Given that we have used three consecutive string elements, skip forward to avoid stuffing in duplicate strings.
 	}
 
 }
